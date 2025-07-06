43dd6ded13013860fcd83c790d5bdd720dfecfb2
tayoky <tayoky4848@gmail.com>
2025-07-06 09:08:01 +0200

    cat : add support for various options

--- src/cat.c
+++ src/cat.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <ctype.h>
 #include "stdopt.h"
 
+#define FLAG_TABS   0x08
+#define FLAG_ENDS   0x10
+#define FLAG_NUMBER 0x20
+#define FLAG_NOPRNT 0x40
+
 struct opt opts[] = {
+	OPT('A',"--show-all",FLAG_NOPRNT | FLAG_ENDS | FLAG_TABS,"equivalent to -vET"),
+	OPT('e',NULL,FLAG_NOPRNT | FLAG_ENDS,"equivalent to -vE"),
+	OPT('E',"--show-ends",FLAG_ENDS,"display $ at end of each line"),
+	OPT('n',"--number",FLAG_NUMBER,"number each line"),
+	OPT('t',NULL,FLAG_NOPRNT | FLAG_TABS,"equivalent to -vT"),
+	OPT('T',"--show-tabs",FLAG_TABS,"display tabs characters as ^I"),
+	OPT('u',NULL,0,"ignored (just kept here for posix)"),
+	OPT('v',"--show-noprinting",FLAG_NOPRNT,"display non printable characters with ^ notation (except for NL and TAB)"),
 };
 
-int ret = 0;
+int ret;
 
 const char *usage = "cat [FILES] ...\n"
 "concatenate files and print to stdout\n"
@@ -24,17 +38,34 @@ void cat(const char *path){
 
 	//handle error
 	if(file == NULL){
-		iprintf("%s : %s\n",path,strerror(errno));
+		perror(path);
 		ret = 1;
 		return;
 	}
 
+	int c;
+	int prev = EOF;
+	int line = 0;
+	while((c = fgetc(file)) != EOF){
+		if((flags & FLAG_NUMBER) && (prev == EOF || prev == '\n'))printf("%6d  ",line++);
+		if(c == '\n' && (flags & FLAG_ENDS)){
+			putchar('$');
+		} else if(c == '\t' && (flags & FLAG_TABS)){
+			putchar('^');
+			putchar('\t' - 1 + 'A');
+			prev = c;
+			continue;
+		}
 
-	char buffer[4096];
-	size_t size;
-	while((size = fread(buffer,1,sizeof(buffer),file))){
-		fwrite(buffer,size,1,stdout);
+		if(!isprint(c) && c != '\n' && c != '\t'){
+			putchar('^');
+			putchar(c - 1 + 'A');
+		} else {
+			putchar(c);
+		}
+		prev = c;
 	}
+
 
 
 	//don't close stdin
@@ -45,13 +76,15 @@ void cat(const char *path){
 
 int main(int argc,char **argv){
 	int i = parse_arg(argc,argv,opts,arraylen(opts));
-
-	for(;i<argc;i++){
-		cat(argv[i]);
-	}
+	ret = 0;
 	//if nothing stdin by default
-	if(argc - i <= 0){
+	if(argc ==  i){
 		cat("-");
+	} else {
+		for(;i<argc;i++){
+			cat(argv[i]);
+		}
 	}
+	
 	return ret;
 }
