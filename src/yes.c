eeed4556f3854582c263a47942530de7760b7e39
AYn0nyme <aynoblox@gmail.com>
2025-07-14 09:57:01 -0400

    yes: print all args+refactor

--- src/yes.c
+++ src/yes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "stdopt.h"
 
 
@@ -6,19 +7,14 @@ const char *usage = "yes [STRING]\nor yes OPTION\n"
 "repeatedly print a string to stdout\n";
 
 int main(int argc,char **argv){
-	if(argc > 2){
-		iprintf("too much argument\n");
-		return 1;
-	}
-
 	parse_arg(argc,argv,NULL,0);
 
-	const char *str = "y";
-	if(argc == 2){
-		str = argv[1];
-	}
-
-	for(;;){
-		puts(str);
-	}
+  for(;;) {
+    if(argc > 1) {
+      for(int c = 1; c < argc; c++) printf("%s ", argv[c]);
+    } else {
+      printf("y");
+    }
+    puts("");
+  }
 }
