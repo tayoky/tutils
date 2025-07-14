f6574d5ac4112ae16af5de853d110f389bb63347
tayoky <tayoky4848@gmail.com>
2025-07-14 17:00:42 +0200

    yes : fix #1

--- src/yes.c
+++ src/yes.c
@@ -6,19 +6,16 @@ const char *usage = "yes [STRING]\nor yes OPTION\n"
 "repeatedly print a string to stdout\n";
 
 int main(int argc,char **argv){
-	if(argc > 2){
-		iprintf("too much argument\n");
-		return 1;
-	}
-
 	parse_arg(argc,argv,NULL,0);
 
-	const char *str = "y";
-	if(argc == 2){
-		str = argv[1];
-	}
-
 	for(;;){
-		puts(str);
+		if(argc > 1){
+			for(int i=1;i<argc;i++){
+				fputs(argv[i],stdout);
+				putchar(i == argc-1 ? '\n' : ' ');
+			}
+		} else {
+			puts("y");
+		}
 	}
 }
