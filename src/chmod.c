6b8885db258861f89882e295a00fa68f88564e4b
tayoky <tayoky4848@gmail.com>
2025-08-03 16:39:06 +0200

    chmod : chmod rewrite

--- src/chmod.c
+++ src/chmod.c
@@ -3,12 +3,10 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <ctype.h>
 #include "stdopt.h"
 
 #define FLAG_RECURSIVE 0x08
-#define FLAG_ADD       0x10
-#define FLAG_SUB       0x20
-#define FLAG_SET       0x40
 
 int ret;
 
@@ -27,7 +25,7 @@ const char *usage = "chown [OPTIONS]... MODE[,MODE]... FILES...\n"
 "multiples MODE can be given separated by comma\n"
 "change mode of files\n";
 
-int ch(mode_t set,mode_t add,mode_t sub,const char *path){
+int ch(const char *mode,const char *path){
 	struct stat st;
 	if(stat(path,&st) < 0){
 		ret = 1;
@@ -35,15 +33,79 @@ int ch(mode_t set,mode_t add,mode_t sub,const char *path){
 		return -1;
 	}
 
-	if(flags & FLAG_SET){
-		st.st_mode = set;
-	}
-	if(flags & FLAG_ADD){
-		st.st_mode |= add;
-	}
-	if(flags & FLAG_SUB){
-		st.st_mode &= ~sub;
-	}
+	const char *m = mode;
+	do{
+		mode_t mask = S_IRWXU | S_IRWXG | S_IRWXO | S_ISUID | S_ISGID;
+		switch(*m){
+		case 'a':
+			m++;
+			break;
+		case 'u':
+			mask = S_IRWXU | S_ISUID;
+			m++;
+			break;
+		case 'g':
+			mask = S_IRWXG | S_ISGID;
+			m++;
+			break;
+		case 'o':
+			mask = S_IRWXO;
+			m++;
+			break;
+		}
+		mask |= S_ISVTX;
+		int op = *m;
+		m++;
+		if(isdigit(op)){
+			op = '=';
+			m--;
+		}
+		if(op != '=' && op != '+' && op != '-'){
+			error("invalid mode '%s'",mode);
+		}
+		mode_t mode = 0;
+		if(isdigit(*m)){
+			char *end;
+			mode = (mode_t)strtol(m,&end,8);
+			m = end;
+		}
+		else for(;;){
+			switch(*m){
+			case 'r':
+				mode |= S_IRUSR | S_IRGRP | S_IROTH;
+				break;
+			case 'w':
+				mode |= S_IWUSR | S_IWGRP | S_IWOTH;
+				break;
+			case 'x':
+				mode |= S_IXUSR | S_IXGRP | S_IXOTH;
+				break;
+			case 's':
+				mode |= S_ISUID | S_ISGID;
+				break;
+			case 't':
+				mode |= S_ISVTX;
+				break;
+			default:
+				goto finish_m;
+			}
+			m++;
+		}
+finish_m:
+		mode &= mask;
+		switch(op){
+		case '=':
+			st.st_mode = mode;
+			break;
+		case '+':
+			st.st_mode |= mode;
+			break;
+		case '-':
+			st.st_mode &= ~mode;
+			break;
+		}
+		if(*m == ',')m++;
+	}while(*m);
 
 	if(chmod(path,st.st_mode) < 0){
 		ret = 1;
@@ -66,7 +128,7 @@ int ch(mode_t set,mode_t add,mode_t sub,const char *path){
 			}
 			char full_path[strlen(path) + strlen(ent->d_name) + 2];
 			sprintf(full_path,"%s/%s",path,ent->d_name);
-			ch(set,add,sub,full_path);
+			ch(mode,full_path);
 			if(!ret)break;
 		}
 
@@ -79,109 +141,15 @@ int ch(mode_t set,mode_t add,mode_t sub,const char *path){
 int main(int argc,char **argv){
 	int i = parse_arg(argc,argv,opts,arraylen(opts));
 	
-	if(i >= argc){
-		error("no mode specfied");
-		return 1;
-	} else if(i + 1 >= argc){
-		error("no files specfied");
+	if(i + 1 >= argc){
+		error("missing argument");
 		return 1;
 	}
-
-	mode_t set,add,sub = 0;
-	for(int j=0;; j++){
-		mode_t mask = S_IRWXU | S_IRWXG | S_IRWXO | S_ISVTX | S_ISUID | S_ISGID;
-		switch(argv[i][j]){
-		case 'a':
-			j++;
-			break;
-		case 'u':
-			mask = S_IRWXU | S_ISUID;
-			j++;
-			break;
-		case 'g':
-			mask = S_IRWXG | S_ISGID;
-			j++;
-			break;
-		case 'o':
-			mask = S_IRWXO;
-			j++;
-			break;
-		}
-
-		mode_t *to_set;
-		switch(argv[i][j]){
-		case '+':
-			flags |= FLAG_ADD;
-			to_set = &add;
-			break;
-		case '-':
-			flags |= FLAG_SUB;
-			to_set = &sub;
-			break;
-		case '=':
-			flags |= FLAG_SET;
-			to_set = &set;
-			break;
-		default:
-			error("invalid mode : '%s'",argv[i]);
-			return 1;
-		}
-		j++;
-
-		mode_t mode = 0;
-		for(;argv[i][j] && argv[i][j] != ',';j++){
-			switch(argv[i][j]){
-			case 'r':
-				mode |= S_IRUSR | S_IRGRP | S_IROTH;
-				break;
-			case 'w':
-				mode |= S_IWUSR | S_IWGRP | S_IWOTH;
-				break;
-			case 'x':
-				mode |= S_IXUSR | S_IXGRP | S_IXOTH;
-				break;
-			case 's':
-				mode |= S_ISUID | S_ISGID;
-				break;
-			case 't':
-				mode |= S_ISVTX;
-				break;
-			case '+':
-				*to_set |= mode & mask;
-				flags |= FLAG_ADD;
-				to_set = &add;
-				break;
-			case '-':
-				*to_set |= mode & mask;
-				flags |= FLAG_SUB;
-				to_set = &sub;
-				break;
-			case '=':
-				*to_set |= mode & mask;
-				flags |= FLAG_SET;
-				to_set = &set;
-				break;
-
-			default:
-				error("invalid mode : '%s'",argv[i]);
-				return 1;
-			}
-		}
-		*to_set |= mode & mask;
-		if(argv[i][j] == ','){
-			j++;
-		} else {
-			break;
-		}
-	}
-
-
 	i++;
-
 	ret = 0;
 
 	while(i < argc){
-		ch(set,add,sub,argv[i]);
+		ch(argv[1],argv[i]);
 		i++;
 	}
 	return ret;
